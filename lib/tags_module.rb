66a3c20e2f76ae00b9b54dd15c00221cea027b92
Max Antonucci <maxx1128@gmail.com>
2019-04-09 20:58:04 -0400

    More blocked tags

--- lib/tags_module.rb
+++ lib/tags_module.rb
@@ -1,6 +1,6 @@
 module Tags
-  SFW = "+-skin_tight+-ass+-cleavage+-underwear+-thigh-highs+-lingerie+-bondage+-huge_breasts+-miniskirt+-impossible_clothes+-swimsuit+-bikini+-kiss+-panties+-panty*+-breast_*".freeze
-  TYPES = "+-comic+-translation_request+-manga+-simple_background".freeze
+  SFW = "+-skin_tight+-ass+-cleavage+-underwear+-thigh-highs+-lingerie+-bondage+-nude+-sideboob+-hands_on_breasts+-see-through+-thighs+-huge_breasts+-miniskirt+-impossible_clothes+-swimsuit+-bikini+-kiss+-panties+-panty*+-breast_*".freeze
+  TYPES = "+-comic+-translation_request+-manga".freeze
   SIZING = "+height%3a>500+width%3A>1000".freeze
 
   def reading_tags
