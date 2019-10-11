4dc1b5b25505a52583935765001290f7df59504c
Max Antonucci <maxx1128@gmail.com>
2019-10-11 10:57:35 -0400

    Add endpoint for image url

--- lib/api.rb
+++ lib/api.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require "sinatra/json"
 require "sinatra/reloader"
 require_relative 'wallpaper_url_query.rb'
 require_relative 'api_params_bridge.rb'
@@ -9,4 +10,12 @@ get '/' do
   image_url = WallpaperUrlQuery.new(tags).random_image
 
   redirect image_url
+end
+
+get '/url/' do
+  parameters = APIParamsBridge.new(params)
+  tags = parameters.give_tags
+  image_url = WallpaperUrlQuery.new(tags).random_image
+
+  json url: image_url
 end
